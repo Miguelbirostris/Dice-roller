0d11199a1008661611ddf8e0757cc993b52ceb52
Miguelbirostris <105656837+Miguelbirostris@users.noreply.github.com>
2025-07-02 17:15:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-let myinput = document.getElementById("my-input")
 
-function stepper(btn){
-    let id = btn.getAttribute("id");
-    let val = myinput.getAttribute("value");
-    let total = (id == "diminuir") ? (parseInt(val)-1): (parseInt(val)+1);
-    myinput.setAttribute("value", total);
-}
 function rolar(){
     let rolagem = Math.floor(Math.random() * 20 +1);
-    let valor = myinput.getAttribute("value");
 
-    spin()
+    spin();
     document.getElementById("result").innerHTML = rolagem;
-    document.getElementById("span").innerHTML = parseInt(valor)+rolagem;
+    document.getElementById("span").innerHTML = rolagem; // No modifier
+
     if (rolagem === 20){
         document.getElementById("result").style.color = "var(--green)";
         document.getElementById("body").style.backgroundColor = "var(--dark-green)";
